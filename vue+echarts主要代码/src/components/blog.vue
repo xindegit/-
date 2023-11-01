9f36a74b54bdf2c4506681cd02cea27129b5db4f
七友 <147929757+xindegit@users.noreply.github.com>
2023-11-01 12:38:37 +0800

    Create blog.vue

---
+++ vue+echarts主要代码/src/components/blog.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref,onMounted } from 'vue'
+import * as echarts from 'echarts';
+import axios from 'axios';
+let dataList = ref([])
+onMounted(async () => {
+    const { data } = await axios.get('/api/blog');
+    dataList.value.push(...data.result)
+    console.log(dataList.value)
+
+})
+
+</script>
+<template>
+<u>
+    <li v-for="(item,index) in dataList" :key="index">{{ item.title }}</li>
+</u>
+</template>
+<style scoped>
+
+</style>
